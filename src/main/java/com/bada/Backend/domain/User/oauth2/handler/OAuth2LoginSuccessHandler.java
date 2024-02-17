7212c39c2419f53e20c3efe99b1cafe4e5644272
Kimsangwon <doncham98@gmail.com>
2024-02-17 17:40:16 +0900

    feat : 프론트 4000번 쓴다고 해서 설정 바꾸고 소셜 반환값 설정
    userKey 라는 키 값에 유저의 기본키를 넣어둠

--- src/main/java/com/bada/Backend/domain/User/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/bada/Backend/domain/User/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -1,9 +1,11 @@
 package com.bada.Backend.domain.User.oauth2.handler;
 
 import com.bada.Backend.domain.User.Role;
+import com.bada.Backend.domain.User.entity.User;
 import com.bada.Backend.domain.User.jwt.service.JwtService;
 import com.bada.Backend.domain.User.oauth2.CustomOAuth2User;
 import com.bada.Backend.domain.User.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +17,8 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -23,6 +27,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -49,9 +54,22 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 log.info("회원가입 추가 정보 입력 폼으로 리다이렉트");
 
                 jwtService.sendAccessAndRefreshToken(response, accessToken, null);
-//                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
-//                                .orElseThrow(() -> new IllegalArgumentException("이메일에 해당하는 유저가 없습니다."));
-//                findUser.authorizeUser();
+                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
+                                .orElseThrow(() -> new IllegalArgumentException("이메일에 해당하는 유저가 없습니다."));
+                findUser.authorizeUser();
+
+                // User PK를 JSON 객체로 변환
+                Map<String, String> userKeyMap = new HashMap<>();
+                userKeyMap.put("userKey", findUser.getId().toString());
+
+                // JSON 문자열로 변환
+                String userKeyJson = objectMapper.writeValueAsString(userKeyMap);
+
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.setCharacterEncoding("UTF-8");
+
+                response.setContentType("application/json;charset=UTF-8"); // 컨텐트 타입을 application/json으로 설정
+                response.getWriter().write(userKeyJson); // JSON 문자열 응답으로 반환
             } else {
                 loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
             }
--- src/main/java/com/bada/Backend/domain/config/SecurityConfig.java
+++ src/main/java/com/bada/Backend/domain/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4000"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
